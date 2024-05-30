68e34e77a7684f6456f6905c293797803a51877f
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2024-05-30 19:47:00 -0300

    Atualização do prisma, criação da tabela User

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+/* eslint-disable indent */
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Henrique Cunha',
+    email: 'rick@email.com',
+  }
+})
