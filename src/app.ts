7276f02eaa3b18ffa31fa25315ecb9341259d6d3
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2024-06-01 01:16:28 -0300

    divisão da rotina de registro de usuário

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,9 @@
 /* eslint-disable indent */
 import fastify from 'fastify'
-import { z } from 'zod'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
-const prisma = new PrismaClient()
 
 
+app.register(appRoutes)
 
-app.post('/users', async (request, reply) => {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
-
-  const { name, email, password } = registerBodySchema.parse(request.body)
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    }
-  })
-  return reply.status(201).send()
-})
---
+++ src/http/controllers/register-controller.ts
@@ -0,0 +1,27 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { Register } from '@/services/register'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+  try {
+    await Register({
+      name,
+      email,
+      password
+    })
+
+  } catch (error) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
