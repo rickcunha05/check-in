b6b5164ac05a8a60a838e77d1349afa55b2bed37
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2024-05-31 23:31:34 -0300

    Criação da rota para criar usuários

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,28 @@
 /* eslint-disable indent */
 import fastify from 'fastify'
+import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
-
 const prisma = new PrismaClient()
 
-prisma.user.create({
-  data: {
-    name: 'Henrique Cunha',
-    email: 'rick@email.com',
-  }
+
+
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    }
+  })
+  return reply.status(201).send()
 })
