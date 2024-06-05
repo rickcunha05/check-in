71ba5f6cc513d8e0135f1d1c07ad386f09e0cf1d
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2024-06-04 21:18:57 -0300

    refactor

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import { app } from './app'
 import { env } from './env'
 
 app.listen({
-    host: '0.0.0.0',
-    port: env.PORT,
+  host: '0.0.0.0',
+  port: env.PORT,
 }).then(() => {
-    console.log('🚀 server running on port 3333')
+  console.log('🚀 server running on port 3333')
 })
